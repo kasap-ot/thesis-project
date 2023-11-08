e8dc9bde1425a1ef79be628c29d017f268973981
kasap-ot <63372251+kasap-ot@users.noreply.github.com>
2023-11-08 18:46:33 +0100

    Update route return values

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+import random
 
 app = FastAPI()
 templates = Jinja2Templates(directory='templates')
 
+def random_color():
+    colors = ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark']
+    return random.choice(colors)
 
-@app.get('/welcome')
+templates.env.filters['random_color'] = random_color
+
+
+@app.get('/')
 async def welcome(request: Request):
     return templates.TemplateResponse('welcome.html', {'request': request})
 
@@ -16,18 +23,16 @@ async def register(request: Request):
 
 @app.get('/register-student')
 async def register_student(request: Request):
-    # return templates.TemplateResponse('register-student.html', {'request': request})
-    return 'register student'
+    return templates.TemplateResponse('register-student.html', {'request': request})
 
 @app.get('/register-company')
 async def register_company(request: Request):
-    # return templates.TemplateResponse('register-company.html', {'request': request})
-    return 'register company'
+    return templates.TemplateResponse('register-company.html', {'request': request})
 
 
 @app.get('/log-in')
-async def log_in():
-    return 'log in'
+async def log_in(request: Request):
+    return templates.TemplateResponse('log-in.html', {'request': request})
 
 
 @app.get('/home-students')
@@ -41,18 +46,30 @@ async def home_companies():
 
 
 @app.get('/offers')
-async def offers():
-    return 'offers'
+async def offers(request: Request):
+    return templates.TemplateResponse('offers.html', {'request': request})
+
+
+@app.get('/offer')
+async def offer(request: Request):
+    return templates.TemplateResponse('offer.html', {'request': request})
 
 
 @app.get('/applications')
-async def applications():
-    return 'applications'
+async def applications(request: Request):
+    return templates.TemplateResponse('applications.html', {'request': request})
 
 
 @app.get('/applicants')
-async def applicants():
-    return 'applicants'
+async def applicants(request: Request):
+    return templates.TemplateResponse(
+        'applicants.html', 
+        {
+            'request': request,
+            'limit': 6,
+            'colors': ['danger', 'danger', 'warning', 'success', 'success', 'success']
+        }
+    )
 
 
 @app.get('/student-profile')
