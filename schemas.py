fb17b9233d32ef0d89b007e3cd12169563e3e6ae
kasap-ot <63372251+kasap-ot@users.noreply.github.com>
2023-11-18 17:09:23 +0100

    Add schemas & other minor changes

---
+++ schemas.py
@@ -0,0 +1,57 @@
+from pydantic import BaseModel
+from datetime import date
+from enums import Status
+from uuid import UUID
+
+
+class Base(BaseModel):
+    id: UUID
+
+
+class Country(Base):
+    name: str
+
+
+class User(Base):
+    email: str
+    password: str
+    name: str
+    country_id: int
+
+
+class Company(User):
+    field: str
+    num_employees: int
+    year_founded: int
+    website: str
+
+
+class Student(User):
+    university: str
+    major: str
+    credits: int
+
+
+class Experience(Base):
+    from_date: date
+    to_date: date
+    company: str
+    description: str
+
+
+class Offer(Base):
+    country_id: int
+    salary: int
+    num_weeks: int
+    field: str
+    deadline: date
+    company_id: int
+    requirements: str
+    responsibilities: str
+    # min_year: int (not necessary?)
+
+
+class Application(Base):
+    student_id: int
+    offer_id: int
+    status: Status
