ef56ba9a02b4492dbc706a0b24ec5e97755f3db3
kasap-ot <63372251+kasap-ot@users.noreply.github.com>
2023-11-22 15:34:16 +0100

    Add user models from SQLModel

--- models.py
+++ models.py
@@ -1,56 +1,85 @@
-from pydantic import BaseModel
-from datetime import date
-from enums import Status
+from sqlmodel import SQLModel, Field
 
 
-class Base(BaseModel):
+class UserBase(SQLModel):
+    email: str = Field(unique=True, index=True)
+    name: str
+    age: int
+
+
+class User(UserBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    hashed_password: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserRead(UserBase):
     id: int
 
 
-class Country(Base):
-    name: str
+class UserUpdate(SQLModel):
+    email: str | None = None
+    name: str | None = None
+    age: int | None = None
 
 
-class User(Base):
-    email: str
-    password: str
-    name: str
-    country_id: int
+
+# from pydantic import BaseModel
+# from datetime import date
+# from enums import Status
 
 
-class Company(User):
-    field: str
-    num_employees: int
-    year_founded: int
-    website: str
+# class Base(BaseModel):
+#     id: int
 
 
-class Student(User):
-    university: str
-    major: str
-    credits: int
+# class Country(Base):
+#     name: str
 
 
-class Experience(Base):
-    from_date: date
-    to_date: date
-    company: str
-    description: str
+# class User(Base):
+#     email: str
+#     password: str
+#     name: str
+#     country_id: int
 
 
-class Offer(Base):
-    country_id: int
-    salary: int
-    num_weeks: int
-    field: str
-    deadline: date
-    company_id: int
-    requirements: str
-    responsibilities: str
-    # min_year: int (not necessary?)
+# class Company(User):
+#     field: str
+#     num_employees: int
+#     year_founded: int
+#     website: str
 
 
-class Application(Base):
-    student_id: int
-    offer_id: int
-    status: Status
+# class Student(User):
+#     university: str
+#     major: str
+#     credits: int
+
+
+# class Experience(Base):
+#     from_date: date
+#     to_date: date
+#     company: str
+#     description: str
+
+
+# class Offer(Base):
+#     country_id: int
+#     salary: int
+#     num_weeks: int
+#     field: str
+#     deadline: date
+#     company_id: int
+#     requirements: str
+#     responsibilities: str
+#     # min_year: int (not necessary?)
+
+
+# class Application(Base):
+#     student_id: int
+#     offer_id: int
+#     status: Status
