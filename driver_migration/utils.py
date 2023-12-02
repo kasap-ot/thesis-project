2434e79d8ce47143cf836aec47bb4168d98867e5
kasap-ot <63372251+kasap-ot@users.noreply.github.com>
2023-12-02 02:59:35 +0100

    Move utility function

--- driver_migration/utils.py
+++ driver_migration/utils.py
@@ -1,4 +1,12 @@
+import string
+import random
 from passlib.context import CryptContext
 
 """ Used to access password hashing utilities. """
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def random_string(length=10):
+    characters = string.ascii_letters + string.digits
+    random_string = ''.join(random.choice(characters) for _ in range(length))
+    return random_string
