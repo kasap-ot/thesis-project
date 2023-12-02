2ac4c9bad457162c7e79bf78df93e26170a4b87f
kasap-ot <63372251+kasap-ot@users.noreply.github.com>
2023-12-02 02:59:13 +0100

    Migrate database schema

--- driver_migration/sql_scripts/add_relationships.sql
+++ driver_migration/sql_scripts/add_relationships.sql
@@ -9,3 +9,25 @@ student_id INT NOT NULL REFERENCES students(id)
 ON DELETE SET NULL
 ON UPDATE CASCADE;
 
+
+-- Migration
+
+ALTER TABLE offers
+DROP CONSTRAINT IF EXISTS offers_company_id_fkey;
+
+ALTER TABLE offers
+ADD CONSTRAINT offers_company_id_fkey
+FOREIGN KEY (company_id)
+REFERENCES companies(id)
+ON DELETE SET DEFAULT
+ON UPDATE CASCADE;
+
+ALTER TABLE offers
+ALTER COLUMN company_id SET DEFAULT -1;
+
+
+ALTER TABLE students
+ADD CONSTRAINT students_unique_email_constraint UNIQUE (email);
+
+ALTER TABLE companies
+ADD CONSTRAINT companies_unique_email_constraint UNIQUE (email);
