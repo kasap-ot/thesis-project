53c41143e58b933ba7d9333e0d601f642f3c784c
kasap-ot <63372251+kasap-ot@users.noreply.github.com>
2024-08-24 18:00:36 +0200

    Set up working integration test

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,47 +1,33 @@
-from datetime import date
-from ..main import app as my_app
-from httpx import AsyncClient
 import pytest
-import pytest_asyncio
-from asgi_lifespan import LifespanManager
+import requests
+from http import HTTPStatus
 
 
-def create_student():
+BASE_URL = "http://localhost:8000/"
+
+
+def create_student() -> dict:
     return {
         "email": "new@student.com",
         "name": "John Doe",
-        "date_of_birth": "2012-10-10",
+        "date_of_birth": "2012-10-01",
         "university": "Stanford",
         "major": "Physics",
         "credits": 150,
         "gpa": 8.00,
-        "password": "123",
+        "password": "fake password",
     }
 
 
-@pytest_asyncio.fixture(scope="module")
-async def app():
-    async with LifespanManager(my_app) as manager:
-        yield manager.app
+@pytest.fixture(scope="module")
+def reset_database():
+    ...
 
 
-@pytest_asyncio.fixture(scope="module")
-async def client(app):
-    async with AsyncClient(app=app, base_url="http://localhost:8000") as client:
-        yield client
-
-
-@pytest.mark.asyncio
-async def test_home(client: AsyncClient):
-    response = await client.get("/test")
-    assert response.status_code == 200
-    assert response.json() == "This is a test."
-
-
-@pytest.mark.asyncio
-async def test_student_pots(client: AsyncClient):
-    student = create_student()
-    response = await client.post("/students", json=student)
-    print(response.json())
-    assert response.status_code == 201
-    print("OK")
+def test_tmp():
+    url = "http://localhost:8000/students"
+    response = requests.post(
+        url=url,
+        json=create_student(),
+    )
+    assert response.status_code == HTTPStatus.CREATED.value
